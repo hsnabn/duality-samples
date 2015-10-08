034e72a76ac3cfe7bbc9291054d64ca315de8c7b
Aban Hussain <hsnabn@gmail.com>
2015-10-08 23:36:37 +0500

    Incomplete FSM implementation of moving. Don't use.
    Going to keep this one in the repository for future reference/improvement.

---
+++ TopDownMovement/InputManager.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Duality;
+using Duality.Input;
+
+namespace TopDownMovement
+{
+    /// <summary>
+    /// This component allows us to listen for input in the scene,
+    /// and act upon the input accordingly.
+    /// </summary>
+    public class InputManager : Component, ICmpUpdatable, ICmpInitializable
+    {
+        // The Movable component; it is assigned directly below this.
+        private Movable movable;
+
+        // OnInit allows us to perform actions on component activations.
+        // We will assign the Movable component to it's variable here,
+        // in the Activation Context.
+        // The input keys are also assigned here.
+        void ICmpInitializable.OnInit(Component.InitContext context)
+        {
+            // If tit is the Activation Context, assign the Movable.
+            if (context == InitContext.Activate)
+                movable = this.GameObj.GetComponent<Movable>();
+        }
+
+        // We can do actions each frame update in OnUpdate.
+        // This is where we will check for input.
+        void ICmpUpdatable.OnUpdate()
+        {
+            // If the Movable component was found...
+            if (movable != null)
+            {
+                // We will use named parameters here, to save space and typing.
+                // We can specify only as many parameters as we need, since we
+                // made all the parameters optional.
+
+                // Move forwards...
+                if (DualityApp.Keyboard.KeyHit(Key.Up)) movable.Move(forward: true);
+
+                // Move to the right...
+                if (DualityApp.Keyboard.KeyHit(Key.Right)) movable.Move(right: true);
+
+                // Move to the left...
+                if (DualityApp.Keyboard.KeyHit(Key.Left)) movable.Move(left: true);
+
+                // Move backwards...
+                if (DualityApp.Keyboard.KeyHit(Key.Down)) movable.Move(backward: true);
+            }
+        }
+
+        // This function is unused in this component.
+        void ICmpInitializable.OnShutdown(Component.ShutdownContext context) { }
+    }
+}
--- TopDownMovement/Movable.cs
+++ TopDownMovement/Movable.cs
@@ -1,22 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 using Duality;
 using Duality.Components;
+using Duality.Components.Renderers;
 
 namespace TopDownMovement
 {
+    /// <summary>
+    /// This component gives the GameObject it is attached to the ability
+    /// to move within the scene.
+    /// </summary>
     // This attribute indicates that this component requires another
     // component to be attached to the same GameObject.
     [RequiredComponent(typeof(Transform))]
-
-    ///<summary>
-    /// This component gives the GameObject it is attached to the ability
-    /// to move across the scene.
-    ///</summary>
-    public class Movable : Component, ICmpUpdatable
+    public class Movable : Component
     {
         // An enum containing all the possible moving states for
         // the movable. This is used for the simple Finite State
@@ -32,8 +31,8 @@ namespace TopDownMovement
         // The speed at which this object will move.
         private float moveSpeed = 5f;
 
-        // Declare this component's MoveState
-        private MoveState moveState;
+        // Declare/create this component's MoveState.
+        private MoveState moveState = new MoveState();
 
         /// <summary>
         /// This function is what does all the moving. It's parameters default
@@ -46,28 +45,69 @@ namespace TopDownMovement
         /// <param name="backward">Whether to move backwards.</param>
         public void Move
             (bool forward = false, 
-             bool right = false, 
-             bool left = false, 
-             bool backward = false)
+            bool right = false, 
+            bool left = false, 
+            bool backward = false)
         {
+            // We are going to use a switch statement for moving.
             switch (moveState)
             {
+                // Move forward case.
                 case MoveState.MOVE_FORWARD:
-                    if (forward) this.GameObj.Transform.MoveBy(-Vector2.UnitY * moveSpeed);
-                    break;
+                    if (forward) // If the "forward" variable was set to true...
+                    {
+                        // Move this GameObject...
+                        this.GameObj.Transform.MoveBy(-Vector2.UnitY * moveSpeed);
+                        // Change this GameObject's AnimSpriteRenderer's displayed frame
+                        this.SetGraphics(1);
+                    }
+                    goto case MoveState.MOVE_RIGHT;
 
+                // Move right case.
                 case MoveState.MOVE_RIGHT:
-                    if (right) this.GameObj.Transform.MoveBy(Vector2.UnitX * moveSpeed);
-                    break;
+                    if (right) // If the "right" variable was set to true...
+                    {
+                        // Move this GameObject...
+                        this.GameObj.Transform.MoveBy(Vector2.UnitX * moveSpeed);
+                        // Change this GameObject's AnimSpriteRenderer's displayed frame
+                        this.SetGraphics(2);
+                    }
+                    goto case MoveState.MOVE_LEFT;
 
+                // Move left case.
                 case MoveState.MOVE_LEFT:
-                    if (right) this.GameObj.Transform.MoveBy(-Vector2.UnitX * moveSpeed);
-                    break;
+                    if (left) // If the "left" variable was set to true...
+                    {
+                        // Move this GameObject...
+                        this.GameObj.Transform.MoveBy(-Vector2.UnitX * moveSpeed);
+                        // Change this GameObject's AnimSpriteRenderer's displayed frame
+                        this.SetGraphics(3);
+                    }
+                    goto case MoveState.MOVE_BACKWARD;
 
+                // Move backward case.
                 case MoveState.MOVE_BACKWARD:
-                    if (right) this.GameObj.Transform.MoveBy(Vector2.UnitY * moveSpeed);
+                    if (backward) // If the "backward" variable was set to true...
+                    {
+                        // Move this GameObject...
+                        this.GameObj.Transform.MoveBy(Vector2.UnitY * moveSpeed);
+                        // Change this GameObject's AnimSpriteRenderer's displayed frame
+                        this.SetGraphics(4);
+                    }
                     break;
             }
+        }
+
+        /// <summary>
+        /// This function sets the first animation frame of the AnimSpriteRenderer
+        /// component of the GameObject this component is attached to.
+        /// </summary>
+        /// <param name="firstFrame">The first frame's frame number.</param>
+        private void SetGraphics(int firstFrame)
+        {
+            // Set this GameObject's AnimSpriteRenderer's AnimFirstFrame (basically,
+            // this is the frame that is displayed) to the supplied value.
+            this.GameObj.GetComponent<AnimSpriteRenderer>().AnimFirstFrame = firstFrame;
         }
     }
 }
